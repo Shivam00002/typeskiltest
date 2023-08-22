1d0f4b91f02f35ff1495e0f5af94a84fb89143c7
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-08-22 23:36:23 +0530

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,8 +4,10 @@ interface ContainerProps {
   children: ReactNode;
 }
 
-export const Container: React.FC<ContainerProps> = ({ children }) => {
+export const Container:
+ React.FC<ContainerProps> = ({ children }) => {
   return (
+
     <div className="max-w-[1170px] mx-auto px-3 md:px-3 lg:px-0">{children}</div>
   );
 };
